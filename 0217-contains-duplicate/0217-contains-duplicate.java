e13dc7eefd3096df08e3b8a3e9b6ada237b0fd38
Saran Sarvesh A G <139890047+saran887@users.noreply.github.com>
2025-07-20 18:35:34 +0530

    Time: 17 ms (38.13%), Space: 58.7 MB (34.58%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+ 
+class Solution{
+    
+    public boolean containsDuplicate(int[] nums) {
+        
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int i = 0; i<nums.length;i++){
+            set.add(nums[i]);
+        }
+        
+        // if it contains duplicate then the size of 
+        // set will not equal to the length
+        // of the array
+        if(set.size() < nums.length)
+            return true;
+        
+        return false;
+    }
+    
+	public static void main (String[] args) {
+	    
+	    Solution sol= new Solution();
+	    
+	    int nums[]= {1, 2, 3, 1};
+	    
+	    boolean res = sol.containsDuplicate(nums);
+	    
+	    System.out.println(res);
+	}
+}
